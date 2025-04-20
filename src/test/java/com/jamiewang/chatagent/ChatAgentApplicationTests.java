7ad1a89256c8899cddc6b108a66f71505e0afe3a
Zeming Wang <wzm970527@gmail.com>
2025-04-20 23:09:24 +0800

    Initial commit

---
+++ src/test/java/com/jamiewang/chatagent/ChatAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamiewang.chatagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
